4360ae04ae68c0b2f00a30fad884463ae1bc876b
VALancaster <valentin.kutergin@gmail.com>
2025-09-05 14:21:36 +0300

    Add project files.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SecureAuthPrototype.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        // метод вызывается, когда пользователь заходит на /Account/Login
+        [HttpGet] // ответ на GET-запрос
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+    }
+}
