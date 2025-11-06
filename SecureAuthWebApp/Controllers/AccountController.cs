e6266e3d3a5b071e7af3d0b23948c2e264e770f0
VALancaster <valentin.kutergin@gmail.com>
2025-11-07 02:58:03 +0300

    feat: Add user registration functionality

--- SecureAuthWebApp/Controllers/AccountController.cs
+++ SecureAuthWebApp/Controllers/AccountController.cs
@@ -2,7 +2,7 @@
 using Microsoft.Extensions.Configuration;
 using Npgsql; // для работы с Postgres
 using System.Threading.Tasks;
-
+using SecureAuth.WebApp.Models;
 
 namespace SecureAuth.WebApp.Controllers
 {
@@ -66,5 +66,65 @@ namespace SecureAuth.WebApp.Controllers
 
             return storedHash != null && BCrypt.Net.BCrypt.Verify(password, storedHash);
         }
-    }
+
+        // метод вызывается, когда пользователь заходит на /Account/Register
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        // метод вызывается, когда пользователь нажмет "Зарегестрироватсья" (Атрибут type="submit" при нажатии кнопки заставляет браузер найти родительскую форму <form>, содержащую атрибут method="post", и отправить HTTP POST-запрос
+        [HttpPost]
+        [ValidateAntiForgeryToken] // отклонение подделанных POST-запросов с других сайтов
+        public async Task<IActionResult> Register([FromForm] RegisterViewModel model)
+        {
+            if (ModelState.IsValid) // если данные прошли валидацию (атрибуты в RegisterViewModel)
+            {
+                bool isUsernameTaken = await IsUsernameTaken(model.Username); // проверка, занят ли логин
+                if (isUsernameTaken) // имя пользователя занято
+                {
+                    ModelState.AddModelError(nameof(model.Username), "This username is already occupied.");
+                    return View(model);
+                }
+                string passwordHash = BCrypt.Net.BCrypt.HashPassword(model.Password); // хэширование пароля с помощью BCrypt
+                await CreateUser(model.Username, passwordHash); // сохраняем нового пользователя в БД
+                return RedirectToAction("Login", "Account");
+            }
+            return View(model);
+        }
+
+        private async Task<bool> IsUsernameTaken(string username)
+        {
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+            await using (NpgsqlConnection connection = new NpgsqlConnection(connectionString)) // подключение к БД
+            {
+                await connection.OpenAsync();
+                string sql = "SELECT COUNT(1) FROM \"Users\" WHERE \"Username\" = @Username";
+                await using (NpgsqlCommand cmd = new NpgsqlCommand(sql, connection)) // команда sql, которую хотим выполнить через подключение connection к БД
+                {
+                    cmd.Parameters.AddWithValue("Username", username); // имя пользователя добавляется в строку SQL как параметр (защита от SQL-инъекций)
+                    object result = await cmd.ExecuteScalarAsync(); // выполнение команды 
+                    return (long)result! > 0; // если результат больше 0, значит имя занято
+                }
+            }
+        }
+
+        private async Task CreateUser(string username, string passwordHash)
+        {
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+
+            await using (NpgsqlConnection connection = new NpgsqlConnection(connectionString)) // подключение к БД
+            {
+                await connection.OpenAsync();
+                string sql = "INSERT INTO \"Users\" (\"Username\", \"PasswordHash\") VALUES (@Username, @PasswordHash)";
+                await using (NpgsqlCommand cmd = new NpgsqlCommand(sql, connection)) // команда sql, которую хотим выполнить через подключение connection к БД
+                {
+                    cmd.Parameters.AddWithValue("Username", username); // имя пользователя добавляется в строку SQL как параметр (защита от SQL-инъекций)
+                    cmd.Parameters.AddWithValue("PasswordHash", passwordHash); // хэш пароля добавляется в строку SQL как параметр (защита от SQL-инъекций)
+                    await cmd.ExecuteNonQueryAsync(); // выполнение команды
+                }
+            }
+        }
+    };
 }
---
+++ SecureAuthWebApp/Models/RegisterViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SecureAuth.WebApp.Models
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "The username is required")]
+        [Display(Name = "Username")]
+        public string Username { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "The password is required")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; } = string.Empty;
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm the password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+}
